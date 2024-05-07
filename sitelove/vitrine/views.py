817c2171488d30ca46469042952a422b237c896d
Ndawa eclador <ecladorvynil507@gmail.com>
2024-05-07 14:13:26 +0100

    configurationsdes routes et views

--- sitelove/vitrine/views.py
+++ sitelove/vitrine/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, "vitrine/home.html")
+
+
+def about(request):
+    return render(request, "vitrine/about.html")
+
+
+def contact(request):
+    return render(request, "vitrine/contact.html")
